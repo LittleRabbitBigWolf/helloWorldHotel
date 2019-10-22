82dd029c4ebc9525184ae876d02495c00390eff4
Logan Mace <loganmace.dev@gmail.com>
2019-10-21 20:48:34 -0500

    Fixed the dates list forr rendering

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,55 +1,39 @@
-import React, { Component } from "react";
-import { PropTypes } from "prop-types";
-import eachDayOfInterval from "date-fns/eachDayOfInterval";
+import React, { Component } from 'react';
+import { PropTypes } from 'prop-types';
+import eachDayOfInterval from 'date-fns/eachDayOfInterval';
 
 export default class Meals extends Component {
   render() {
-    const dateRange = this.props.dateString.split(" to ");
-    // console.log("look1", dateRange);
+    const dateRange = this.props.dateString.split(' to ');
     const dates =
+      // TODO: Refactor the length handling.
       dateRange.length === 2 &&
       dateRange[1].length === 10 &&
       eachDayOfInterval({
-        start: new Date(dateRange[0].split("-")),
-        end: new Date(dateRange[1].split("-"))
+        start: new Date(dateRange[0].split('-')),
+        end: new Date(dateRange[1].split('-'))
       });
-    console.log("dates", dates);
 
-    // const mealPlan =
-    //   this.props.dates &&
-    //   this.props.dates.map(dateRange => {
-    //     return (
-    //       <div className="col-xs-12  col-sm-12 col-md-12 col-lg-12">
-    //         <ol id="list">
-    //           <li className="morning">
-    //             Breakfast for {this.props.name} on {dateRange}
-    //           </li>
-    //           <li className="afternoon">
-    //             Lunch for {this.props.name} on {dateRange}
-    //           </li>
-    //           <li className="night">
-    //             Dinner for {this.props.name} on {dateRange}
-    //           </li>
-    //         </ol>
-    //       </div>
-    //     );
-    //   });
-    return (
-      // { mealPlan };
-      <div className="col-xs-12  col-sm-12 col-md-12 col-lg-12">
-        <ol id="list">
-          <li className="morning">
-            Breakfast for {this.props.name} on {this.props.dateString}
-          </li>
-          <li className="afternoon">
-            Lunch for {this.props.name} on {this.props.dateString}
-          </li>
-          <li className="night">
-            Dinner for {this.props.name} on {this.props.dateString}
-          </li>
-        </ol>
-      </div>
-    );
+    const mealPlan =
+      dates.length &&
+      dates.map((dateRange, id) => {
+        return (
+          <div key={id} className="col-xs-12  col-sm-12 col-md-12 col-lg-12">
+            <ol id="list">
+              <li className="morning">
+                Breakfast for {this.props.name} on {dateRange.toDateString()}
+              </li>
+              <li className="afternoon">
+                Lunch for {this.props.name} on {dateRange.toDateString()}
+              </li>
+              <li className="night">
+                Dinner for {this.props.name} on {dateRange.toDateString()}
+              </li>
+            </ol>
+          </div>
+        );
+      });
+    return <div>{mealPlan}</div>;
   }
 }
 
